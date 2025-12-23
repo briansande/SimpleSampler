c7af7047d303baa6181f1ba28631eca0996a3bc0
briansande <sandeb@oregonstate.edu>
2025-12-22 18:57:14 -0600

    SD Card reader added. Files listed, but truncated due to screen size

--- SimpleSampler.cpp
+++ SimpleSampler.cpp
@@ -14,16 +14,84 @@ using MyOledDisplay = OledDisplay<SSD130x4WireSpi128x64Driver>;
 DaisyPod      hw;
 MyOledDisplay display;
 Parameter p_knob1, p_knob2;
+static int32_t  inc;
+
+
+SdmmcHandler   sd;
+FatFSInterface fsi;
+FIL            SDFile;
+DIR            dir;
+FILINFO        fno;
+
+
 
 const uint32_t DISPLAY_FPS = 10;                        //  FPS -- later converted to time in ms
 
+
+void DisplayFilesOnScreen() {
+    char strbuff[128];
+    int fileCount = 0;
+    int lineCount = 0;
+    
+    // Clear display
+    display.Fill(false);
+    display.SetCursor(0, 0);
+    display.WriteString("SD Files:", Font_11x18, true);
+    
+    // Open root directory
+    if(f_opendir(&dir, "/") != FR_OK) {
+        display.SetCursor(0, 16);
+        display.WriteString("SD Error!", Font_11x18, true);
+        display.Update();
+        return;
+    }
+    
+    // Read directory entries
+    while(1) {
+        FRESULT res = f_readdir(&dir, &fno);
+        
+        // Break on error or end of directory
+        if(res != FR_OK || fno.fname[0] == 0) break;
+        
+        // Skip directories and hidden files
+        if(fno.fattrib & (AM_HID | AM_DIR)) continue;
+        
+        // Display filename (limited space on screen)
+        if(lineCount < 3) {  // Show max 3 files at once
+            display.SetCursor(0, 16 + (lineCount * 16));
+            
+            // Truncate filename if too long
+            char displayName[20];
+            strncpy(displayName, fno.fname, 19);
+            displayName[19] = '\0';
+            
+            sprintf(strbuff, "%s", displayName);
+            display.WriteString(strbuff, Font_11x18, true);
+            lineCount++;
+        }
+        
+        fileCount++;
+    }
+    
+    f_closedir(&dir);
+    
+    // Show total count
+    display.SetCursor(0, 48);
+    sprintf(strbuff, "Total: %d files", fileCount);
+    display.WriteString(strbuff, Font_11x18, true);
+    display.Update();
+
+}
 
 
 
 int main(void)
 {
-    hw.Init();
     
+
+    hw.Init();
+    inc     = 0;
+
     uint32_t lastUpdateTime = System::GetNow();             // Initialize lastUpdateTime to the current time
 
     /** Configure then initialize the Display */
@@ -40,11 +108,30 @@ int main(void)
 
     hw.StartAdc();
 
+    /* SD Card Additions */
+    // Init SD Card
+    SdmmcHandler::Config sd_cfg;
+    sd_cfg.Defaults();
+    sd.Init(sd_cfg);
+
+    // Links libdaisy i/o to fatfs driver.
+    fsi.Init(FatFSInterface::Config::MEDIA_SD);
+
+    // Mount SD Card
+    f_mount(&fsi.GetSDFileSystem(), "/", 1);
+
+
+
+
 
 
     char strbuff[128];
     while(1)
     {
+        
+        hw.encoder.Debounce();
+        inc += hw.encoder.Increment();
+
         uint32_t now = System::GetNow();
         r = p_knob1.Process();
         g = p_knob2.Process();
@@ -55,18 +142,20 @@ int main(void)
 
         if(now - lastUpdateTime >= 1000/(DISPLAY_FPS))
         {
-            
+            // Display Testing Code
             sprintf(strbuff, "R:%d G:%d B:%d", (int)(r*255), (int)(g*255), (int)(b*255));
             display.Fill(false);
             display.SetCursor(0, 0);
             display.WriteString(strbuff, Font_11x18, false);
             
-            sprintf(strbuff, "B:%d", (int)lastUpdateTime);
+            sprintf(strbuff, "B:%d", (int)inc);
             display.SetCursor(0, 32);
             display.WriteString(strbuff, Font_11x18, false);
-            
+            // DisplayFilesOnScreen();
+
             display.Update();
             lastUpdateTime = now;
         }
     }
 }
+
